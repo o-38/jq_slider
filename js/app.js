e369a687c494baabbc2bf9cd3320548c0ce5bb26
o-38 <syw.9915.4@gmail.com>
2023-08-18 21:32:57 +0900

    slider

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+  autoplay: true,
+  autoplaySheed: 1000,
+  speed: 1000,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<div class="slick-prev"></div>',
+  nextArrow: '<div class="slick-next"></div>',
+});
